4a272f103d73080127fcb59e858f0919c7db64b8
Andrew Wagenmaker <ajwagen@cs.washington.edu>
2020-05-02 13:55:25 -0700

    Initial Commit

---
+++ accelerometer_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accelerometer_test
+//
+//  Created by Andrew Wagenmaker on 5/2/20.
+//  Copyright © 2020 Andrew Wagenmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
